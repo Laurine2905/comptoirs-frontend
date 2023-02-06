059eb80993224573c891f7b704dbbeb84838f93c
Laurine2905 <laurine.rat@outlook.fr>
2023-02-06 20:01:40 +0100

    finish

--- src/views/ProduitView.vue
+++ src/views/ProduitView.vue
@@ -21,22 +21,22 @@
           <td>{{ produits.unitesCommandees }}</td>
         </tr>
         <td>
-          <button @click="page=1">
+          <button @click="changePage('begin')">
             Premiere page
           </button>
         </td>
         <td>
-          <button @click=page=page-1>
+          <button @click="changePage(-1)">
             Precedente
           </button>
         </td>
         <td>
-          <button @click=page=page-1>
+          <button @click="changePage(1)">
             Suivante
           </button>
         </td>
         <td>
-          <button @click="deleteEntity(categorie._links.self.href)">
+          <button @click="changePage('last')">
             Derniere page
           </button>
         </td>
@@ -64,11 +64,27 @@ let data = reactive({
   listeProduit: []
 });
 
+function changePage(page) {
+  if(page === 'begin'){
+    data.numeroPage = 0;
+    chargeProduits();
+  }
+  else if(page == 'last'){
+    data.numeroPage = data.pageMax;
+    chargeProduits();
+  }
+  if(data.numeroPage + page >= 0 && data.numeroPage + page <= data.pageMax){
+    if(data.numeroPage <= data.pageMax && data.numeroPage >= 0){
+      data.numeroPage = data.numeroPage + page;
+      chargeProduits();
+    }
+  }
+}
 function chargeProduits() {
   // Appel à l'API pour avoir la liste des catégories
   // Trié par code, descendant
   // Verbe HTTP GET par défaut
-  doAjaxRequest(BACKEND + "/api/produits?sort=nom&page="date.page"&size=6")
+  doAjaxRequest(BACKEND + "/api/produits?sort=nom&page="date.page"&size=5")
       .then((json) => {
         data.listeProduit = json._embedded.produits;
       })
